d224a78aff95afadd1c3d1104c84c4e600ecf513
hua <2500935743@qq.com>
2021-10-04 10:16:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @auther: 桦
+ * @date: Created in 2021/10/4-10:11
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
